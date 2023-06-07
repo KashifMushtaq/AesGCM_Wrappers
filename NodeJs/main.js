0b6132f069cb0e60cc4926fa3b670f204cb9a397
Kashif Mushtaq <45662405+KashifMushtaq@users.noreply.github.com>
2023-06-06 23:57:17 -0400

    Changes to fix UTF-8 text length problem

--- NodeJs/main.js
+++ NodeJs/main.js
@@ -112,7 +112,5 @@ if (DecryptedText.v === PlainText.v) {
 }
 
 function lengthInUtf8Bytes(str) {
-  // Matches only the 10.. bytes that are non-initial characters in a multi-byte sequence.
-  var m = encodeURIComponent(str).match(/%[89ABab]/g);
-  return str.length + (m ? m.length : 0);
+    return Buffer.byteLength(str, 'UTF-8');
 }
